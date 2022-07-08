1ab304aefbe3253fa279e541a2c717579704ad27
ongdv <ongdv94@gmail.com>
2022-07-08 14:45:25 +0900

    [INIT] Initialize

---
+++ src/main/java/com/ongdv/fastcampuspractice/FastcampusPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ongdv.fastcampuspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusPracticeApplication.class, args);
+    }
+
+}
